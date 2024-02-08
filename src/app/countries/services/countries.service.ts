377ec58f09edf83e9bdbef1d203690e27cdb5c64
VN - Alvaro Hdz Valero <alvaro.hernandezv@viewnext.com>
2024-02-08 14:33:01 +0100

    S09 - Argumentos por URL - Página del País

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -10,6 +10,18 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
+  searchCountryByAlphaCode( code: string ): Observable<Country[]> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError( error => {
+          console.log(error);
+
+          return of([]);
+        } )
+      );
+  }
+
   searchByCapital( term: string ): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
     return this.http.get<Country[]>(url)
