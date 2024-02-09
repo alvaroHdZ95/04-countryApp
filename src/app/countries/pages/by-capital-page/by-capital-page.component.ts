61227103afcdf8099a2a613959700e8387f7c9c1
VN - Alvaro Hdz Valero <alvaro.hernandezv@viewnext.com>
2024-02-09 10:23:14 +0100

    S10 - Mostrar y ocultar el LoadingSpinner

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -9,14 +9,19 @@ import { Country } from '../../interfaces/country';
 })
 export class ByCapitalPageComponent {
 
-  public countries: Country[] = []
+  public countries: Country[] = [];
+  public isLoading: boolean = false;
 
   constructor( private countriesService: CountriesService) {}
 
   searchByCapital( term:string ):void {
+
+    this.isLoading = true;
+
     this.countriesService.searchByCapital( term )
       .subscribe( countries => {
         this.countries = countries;
+        this.isLoading = false;
       })
   }
 
